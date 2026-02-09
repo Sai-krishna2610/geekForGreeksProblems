17b853c148a2b0389e715b0a41d395446623bb91
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-02-09 12:29:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.js
@@ -0,0 +1,16 @@
+/**
+ * @param number a
+ * @param number b
+ * @returns number
+ */
+
+class Solution {
+    gcd(a, b) {
+        // code here
+        if(b==0)
+        {
+            return a;
+        }
+        return this.gcd(b,a%b);
+    }
+}
