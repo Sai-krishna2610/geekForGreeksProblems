b3061a8957cac96fe38e55cbafdd50fc3142c3a2
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-02-10 14:47:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -0,0 +1,32 @@
+bool comp(pair<int,int> &a, pair<int,int> &b)
+{
+    return a.second < b.second;
+}
+
+class Solution {
+public:
+    int maxMeetings(vector<int>& start, vector<int>& end) {
+
+        int n = start.size();
+
+        vector<pair<int,int>> timer;
+
+        for (int i = 0; i < n; i++) {
+            timer.push_back({start[i], end[i]});
+        }
+
+        sort(timer.begin(), timer.end(), comp);
+
+        int total = 0;
+        int ending = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (timer[i].first > ending) {
+                total++;
+                ending = timer[i].second;
+            }
+        }
+
+        return total;
+    }
+};
