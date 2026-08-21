1204e57b73be3e23633411bcbf2cca1aad233551
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-08-21 22:44:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count ways with 3 moves/count-ways-with-3-moves.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+  public:
+    // Function to count the number of ways in which frog can reach the top.
+    //recursive approach
+    int ways(int n)
+    {
+        if (n==0)
+        return 1;
+        if(n<0)
+        {
+            return 0;
+        }
+        return ways(n-1)+ways(n-2)+ways(n-3);
+    }
+    
+    int countWays(int n) {
+
+        // your code here
+        ways(n);
+    }
+};
